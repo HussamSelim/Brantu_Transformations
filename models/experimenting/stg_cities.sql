339d6b79bde3596833f5e415904ab73c614a9030
Hussam.selim@brantu.com <Hussam.selim@brantu.com>
2022-01-31 14:52:32 +0000

    Experiment staging

---
+++ models/experimenting/stg_cities.sql
@@ -0,0 +1,14 @@
+
+{{ config(materialized='table') }}
+
+with cities as (
+
+    select 
+        _id as id,
+        active as status,
+        name_en as city,
+        code as code,
+        parent_code as parent_code
+
+    from brantu-test.mongo_production_brantu.cities
+)
